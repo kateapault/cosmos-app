07c0f7ba9d1eec117ecbc955c8edd3fda3ecef33
github username <github email address>
2020-02-13 11:27:08 -0500

    Styling

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,5 @@
 class HomepageController < ApplicationController
   def home
+    @rating = [1, 2, 3, 4, 5]
   end
 end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,6 @@
 class IngredientsController < ApplicationController
+  before_action :authorized, except: [:index, :show]
+  
   def index
     @ingredients = Ingredient.all
   end
