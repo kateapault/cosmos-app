ef16779c2e5b0bd658dec56a877c58174ae175d5
github username <kateapault@gmail.com>
2020-06-29 18:54:31 -0400

    formatting ingredient string

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,7 @@
 from urllib import request
+from urllib import error
+import sys
+import re
 
 def hacky_get_names(text):
     names = []
@@ -54,34 +57,100 @@ def liquor_dot_com(page_text):
             steps.append(chunk.split("</p>")[0])
     return ingredients, steps
 
-
+def format_cocktail_name_for_search(name,symbol="-"):
+    return name.replace(" ",symbol).replace("'","").replace("."," ").lower()
 
 def get_recipe(name):
-    cocktail = name.replace(" ","-").lower()
+    cocktail = format_cocktail_name_for_search(name)
     url = f"https://www.liquor.com/recipes/{cocktail}/"
     try: 
         result = request.urlopen(url)
         binary_data = result.read()
         data = binary_data.decode('utf-8')
-        steps, ingredients = liquor_dot_com(data)
+        ingredients, steps = liquor_dot_com(data)
         return {name: {'steps': steps, 'ingredients': ingredients}}
     except:
         print(f"page for {name} was not found")
         return name
-        
 
-url = f"https://en.wikipedia.org/wiki/List_of_cocktails"
-result = request.urlopen(url)
-binary_data = result.read()
-utf8_data = binary_data.decode('utf-8')
-text = utf8_data.split('id="See_also"')[0]
-cocktails = hacky_get_names()
 
-no_recipe_yet = []
-ingredients = 
-
-for cocktail in cocktails:
-    if type(get_recipe(cocktail)) == dict:
-        
+def format_ingredients(original_ingredient_string):
+    ingredient = {
+        'name': '',
+        'category': '',
+        'amount': '',
+        'measurement': ''
+    }
+    #  Garnish
+    if ':' in original_ingredient_string:
+        name = original_ingredient_string.split(':')[1].strip()
+        if 'rim' in name:
+            ingredient['category'] = 'rim'
+            ingredient['name'] = name.split('rim')[0].strip().lower()
+            ingredient['amount'] = '-'
+            ingredient['measurement'] = '-'
+        else:
+            if 'slice' in name:
+                ingredient['name'] = name.split('slice')[0].strip().lower()
+                ingredient['measurement'] = 'slice'
+            elif 'twist' in name:
+                ingredient['name'] = name.split('twist')[0].strip().lower()
+                ingredient['measurement'] = 'twist'
+            elif 'wheel'in name:
+                ingredient['name'] = name.split('wheel')[0].strip().lower()
+                ingredient['measurement'] = 'twist'            
+            else:
+                ingredient['name'] = name.lower()
+                ingredient['measurement'] = 'unit'
+            ingredient['amount'] = '1'    
+            ingredient['category'] = 'garnish'
     else:
-        no_recipe_yet.append(cocktail)
+        if bool(re.search('\d',original_ingredient_string)):
+            if 'oz' in original_ingredient_string:
+                amt, name = original_ingredient_string.split('oz')
+                ingredient['amount'] = amt.replace(' ','-').strip()
+                ingredient['name'] = name.strip().lower()
+                ingredient['measurement'] = 'oz'
+            elif 'ounce' in original_ingredient_string:
+                amt, name = original_ingredient_string.split('ounce')
+                ingredient['amount'] = amt.replace(' ','-')
+                if name[0] == 's' and name[1] == ' ':
+                    name = name[2:]
+                ingredient['name'] = name.lower()
+                ingredient['measurement'] = 'oz'                
+            # elif 'splash'
+        for chunk in ingredient_arr:
+            pass
+            
+            
+    # if there's a number in there
+        # if there's a slash in there
+    # oz = ounce
+
+    # if 'garnish'
+    
+    # if 
+
+
+
+#########################################
+
+def main():
+    url = f"https://en.wikipedia.org/wiki/List_of_cocktails"
+    result = request.urlopen(url)
+    binary_data = result.read()
+    utf8_data = binary_data.decode('utf-8')
+    text = utf8_data.split('id="See_also"')[0]
+    cocktails = hacky_get_names(text)
+
+    no_recipe_yet = []
+    ingredients = []
+
+    i = 0
+    for cocktail in cocktails:
+        info = get_recipe(cocktail)
+        if type(info) == dict:
+            
+        else:
+            no_recipe_yet.append(info)
+
